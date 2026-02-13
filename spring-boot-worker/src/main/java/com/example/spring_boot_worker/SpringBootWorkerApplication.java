715d32432167b70620e9fd0806f19859f5d30340
daiki-kaneda <daiki0622developing@gmail.com>
2026-02-14 01:39:36 +0900

    feat(worker): add spring boot project and move .gitignore to root

---
+++ spring-boot-worker/src/main/java/com/example/spring_boot_worker/SpringBootWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWorkerApplication.class, args);
+	}
+
+}
