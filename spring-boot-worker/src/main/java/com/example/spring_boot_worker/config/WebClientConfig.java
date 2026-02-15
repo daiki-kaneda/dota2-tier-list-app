61d7fa67a290ebec034494e97b250ad782baf96a
daiki-kaneda <daiki0622developing@gmail.com>
2026-02-16 01:21:50 +0900

    feat(config): add web client config

---
+++ spring-boot-worker/src/main/java/com/example/spring_boot_worker/config/WebClientConfig.java
@@ -0,0 +1,15 @@
+package com.example.spring_boot_worker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig{
+    @Bean
+    public WebClient openDotaWebClient(WebClient.Builder builder){
+        return builder
+        .baseUrl("https://api.opendota.com/api")
+        .build();
+    }
+}
