5172c9fa86771238e04d07ab695c8e7aa12d99a2
daiki-kaneda <daiki0622developing@gmail.com>
2026-02-16 00:29:32 +0900

    feat(port): add hero datasource,hero repository

---
+++ spring-boot-worker/src/main/java/com/example/spring_boot_worker/usecase/port/HeroDataSource.java
@@ -0,0 +1,9 @@
+package com.example.spring_boot_worker.usecase.port;
+
+import java.util.List;
+
+import com.example.spring_boot_worker.entity.Hero;
+
+public interface HeroDataSource {
+    List<Hero> fetchAllHeros();
+}
