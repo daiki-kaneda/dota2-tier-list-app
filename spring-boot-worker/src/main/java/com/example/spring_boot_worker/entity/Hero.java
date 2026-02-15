e4a8843d5b96e5cb09560ecad7ffbca934292a35
daiki-kaneda <daiki0622developing@gmail.com>
2026-02-16 00:51:41 +0900

    refactor: make hero immutable

--- spring-boot-worker/src/main/java/com/example/spring_boot_worker/entity/Hero.java
+++ spring-boot-worker/src/main/java/com/example/spring_boot_worker/entity/Hero.java
@@ -1,23 +1,14 @@
 package com.example.spring_boot_worker.entity;
 
-import lombok.Getter;
+import lombok.Value;
 
-@Getter
+@Value
 public class Hero {
-    private String name;
-    private int picks;
-    private int wins;
+    private final String name;
+    private final int picks;
+    private final int wins;
 
-    private Hero(){}
-
-    public double calculateWinRate() {
-        if (picks == 0) {
-            return 0.0;
-        }
-        return (double) wins / picks * 100.0;
-    }
-
-    public static Hero create(String name, int picks, int wins) {
+    public Hero(String name, int picks, int wins) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("名前が空欄です。");
         }
@@ -27,11 +18,16 @@ public class Hero {
         if (picks < wins) {
             throw new IllegalArgumentException("ピック数は勝利数以上でなくてはいけません。");
         }
-        Hero hero = new Hero();
-        hero.name = name;
-        hero.picks = picks;
-        hero.wins = wins;
 
-        return hero;
+        this.name = name;
+        this.picks = picks;
+        this.wins = wins;
+    }
+
+    public double calculateWinRate() {
+        if (picks == 0) {
+            return 0.0;
+        }
+        return (double) wins / picks * 100.0;
     }
 }
