36ebedbac99902e982a52bb919c84ed095a89a6e
daiki-kaneda <daiki0622developing@gmail.com>
2026-02-17 17:00:44 +0900

    feat(config): add dynamodb client,dynamodb enhanced client beans

---
+++ spring-boot-worker/src/main/java/com/example/spring_boot_worker/config/DynamoDbConfig.java
@@ -0,0 +1,25 @@
+package com.example.spring_boot_worker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+@Configuration
+public class DynamoDbConfig {
+    @Bean
+    DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .region(Region.AP_NORTHEAST_1)
+                .build();
+    }
+
+    @Bean
+    DynamoDbEnhancedClient dynamoDbEnhancedClient(DynamoDbClient client) {
+        return DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(client)
+                .build();
+    }
+}
