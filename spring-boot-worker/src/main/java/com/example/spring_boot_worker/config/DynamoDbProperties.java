340acaebb43067e8ae88591c2ee793a0f3b34f30
daiki-kaneda <daiki0622developing@gmail.com>
2026-02-17 17:51:29 +0900

    feat(config): add dynamodb properties

---
+++ spring-boot-worker/src/main/java/com/example/spring_boot_worker/config/DynamoDbProperties.java
@@ -0,0 +1,14 @@
+package com.example.spring_boot_worker.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@ConfigurationProperties(prefix = "app.dynamodb")
+@Data
+@Component
+public class DynamoDbProperties {
+    private String region;
+    private String tableName;
+}
