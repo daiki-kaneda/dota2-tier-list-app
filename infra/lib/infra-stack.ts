abf23741f148d826dec62793dcd76a0aca0fd910
daiki-kaneda <daiki0622developing@gmail.com>
2026-02-17 23:53:47 +0900

    feat(stack): add vpc,worker security group,worker iam role

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,41 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // === VPC ===
+    const vpc = new ec2.Vpc(this, 'MyVPC', {
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          name: 'public',
+          subnetType: ec2.SubnetType.PUBLIC
+        }
+      ]
+    });
+    vpc.addGatewayEndpoint("DynamoDBEndpoint", {
+      service: ec2.GatewayVpcEndpointAwsService.DYNAMODB
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const workerSg = new ec2.SecurityGroup(
+      this, "WorkerSg", {
+      vpc: vpc,
+      allowAllOutbound: true,
+      description: "Security Group for tier list making worker"
+    }
+    );
+
+    const wokerRole = new iam.Role(this, "WorkerRole", {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+      managedPolicies: [
+        // Session Manager を使ってブラウザからログインできるようにする
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')
+      ]
+    });
+
   }
 }
