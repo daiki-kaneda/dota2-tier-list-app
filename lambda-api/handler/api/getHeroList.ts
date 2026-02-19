210a9429168def4bf9c5ad0f8286e59de0b7b723
daiki-kaneda <daiki0622developing@gmail.com>
2026-02-19 17:23:40 +0900

    feat(handler): add get hero list function

--- lambda-api/handler/api/getHeroList.ts
+++ lambda-api/handler/api/getHeroList.ts
@@ -1,8 +1,22 @@
-import {APIGatewayProxyEvent,APIGatewayProxyResult} from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { HeroRepository } from "../../usecase/port/HeroRepository";
 import { DynamoDbHeroRepository } from "../../infrastructure/dynamodb/DynamoDbHeroRepository";
+import { getHeroList } from "../../usecase/getHeroList";
 
-const repo:HeroRepository = new DynamoDbHeroRepository(process.env.TABLE_NAME ?? '');
-export const handler = async (event:APIGatewayProxyEvent):Promise<APIGatewayProxyResult>=>{
+const repo: HeroRepository = new DynamoDbHeroRepository(process.env.TABLE_NAME ?? '');
 
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+        const result = await getHeroList(repo);
+        return {
+            statusCode: 200,
+            body: result ? JSON.stringify(result) : ""
+        }
+    } catch (e) {
+        console.error(e);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Internal Server Error" })
+        }
+    }
 }
