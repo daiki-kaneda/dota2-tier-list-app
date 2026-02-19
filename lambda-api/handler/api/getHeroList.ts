d20cae06a3cafe622333f38e03726fb53485a72e
daiki-kaneda <daiki0622developing@gmail.com>
2026-02-19 17:15:16 +0900

    chore(deps): add types/node dev dependency,tsconfig.json

--- lambda-api/handler/api/getHeroList.ts
+++ lambda-api/handler/api/getHeroList.ts
@@ -1,3 +1,8 @@
 import {APIGatewayProxyEvent,APIGatewayProxyResult} from "aws-lambda";
+import { HeroRepository } from "../../usecase/port/HeroRepository";
+import { DynamoDbHeroRepository } from "../../infrastructure/dynamodb/DynamoDbHeroRepository";
 
-export const handler = async (event:APIGatewayProxyEvent):Promise<APIGatewayProxyResult>=>{}
+const repo:HeroRepository = new DynamoDbHeroRepository(process.env.TABLE_NAME ?? '');
+export const handler = async (event:APIGatewayProxyEvent):Promise<APIGatewayProxyResult>=>{
+
+}
