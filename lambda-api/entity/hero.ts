45ca38480fd1258390866af59c239780ae37fbea
daiki-kaneda <daiki0622developing@gmail.com>
2026-02-19 16:29:43 +0900

    feat(entity): add hero entity

---
+++ lambda-api/entity/hero.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+export const HeroSchema = z.object({
+    createdAt: z.number(),
+    name: z.string().min(1, '名前は必須です'),
+    picks: z.number().min(0, 'ピック数は0以上である必要があります'),
+    wins: z.number().min(0, '勝利数は0以上である必要があります'),
+    tier: z.enum(["S", "A", "B", "C"], 'ティアはS,A,B,Cのいずれかです。'),
+    winRate: z.number()
+        .min(0, '勝率は0以上である必要があります。')
+        .max(100, '勝率は100以下である必要があります。')
+});
+export const HeroListSchema = z.array(HeroSchema);
+
+export type Hero = z.infer<typeof HeroSchema>;
+export type HeroList = z.infer<typeof HeroListSchema>;
