e762eb0d8b9e175693a22dae5b04114ee1fd9500
daiki-kaneda <daiki0622developing@gmail.com>
2026-02-24 17:08:47 +0900

    refactor(ui): add win rate bar

--- frontend/lib/features/hero/presentation/page/hero_list_widget.dart
+++ frontend/lib/features/hero/presentation/page/hero_list_widget.dart
@@ -25,11 +25,46 @@ class HeroListTile extends StatelessWidget {
   final Hero hero;
   @override
   Widget build(BuildContext context) {
-    final formatter = NumberFormat.compact();
     return ListTile(
-      leading: Container(width: 10,color: hero.tier.color,),
+      leading: Container(width: 10, color: hero.tier.color),
       title: Text(hero.name),
-      subtitle: Text("勝率: ${formatter.format(hero.winRate)}%"),
+      subtitle: WinRateWidget(hero),
+    );
+  }
+}
+
+class WinRateWidget extends StatelessWidget {
+  const WinRateWidget(this.hero, {super.key});
+
+  final Hero hero;
+
+  @override
+  Widget build(BuildContext context) {
+     final formatter = NumberFormat.compact();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'WIN RATE: ${formatter.format(hero.winRate)}%',
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 4),
+        WinRateBar(hero),
+      ],
+    );
+  }
+}
+
+class WinRateBar extends StatelessWidget {
+  const WinRateBar(this.hero, {super.key});
+
+  final Hero hero;
+
+  @override
+  Widget build(BuildContext context) {
+    return LinearProgressIndicator(
+      value: hero.winRate / 100,
+      color: hero.tier.color,
     );
   }
 }
